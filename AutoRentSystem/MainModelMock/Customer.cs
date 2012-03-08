38b6578f6ffbe44b3b396e3696b0bb8b2ca14cd1
svlanka6@gmail.com <svlanka6@gmail.com@b45a65a7-9af5-b9b8-5d00-0791a1bd0563>
2012-03-08 10:17:23 +0000

    Add MainModelMock project

---
+++ AutoRentSystem/MainModelMock/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MainModelMock
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Insuarance { get; set; }
+        public string Passport { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AutoRentSystem/MainModelMock/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MainModelMock
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+        public int Seats { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
